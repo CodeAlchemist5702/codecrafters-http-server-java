19d92a40155faba32ae975236adc92ab476c123c
anurag <anuragsharmajobmail@gmail.com>
2025-04-22 23:52:00 +0530

    http program3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
   public static void main(String[] args) {
@@ -20,6 +22,9 @@ public class Main {
         Socket socket = serverSocket.accept();
          BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
          String requestLine = br.readLine();
+         String str="/";
+         int len=0;
+         Map<String, String> headers = new HashMap<>();
          String path ="/";
          System.out.println(requestLine);
          if(requestLine!=null){
@@ -30,13 +35,27 @@ public class Main {
          }
          System.out.println(path);
          if("/".equals(path) || path.contains("/echo/")) {
-             String str="/";
-             int len=0;
              if(path.contains("/echo/")) {
                  str = path.substring(6);
                  len = str.length();
              }
              System.out.println(str+" "+len);
+             String responseBody= "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+len+"\r\n\r\n"+str;
+             System.out.println(responseBody);
+             socket.getOutputStream().write(responseBody.getBytes());
+         }
+         else if("user-agent".equals(path)){
+             String line;
+             while ((line=br.readLine())!=null && !line.isEmpty()){
+                 String []headerParts = line.split(":",2);
+                 // split at first :
+                 if(headerParts.length==2){
+                     // as we have to store in map so checking if it has been split into 2 parts
+                     headers.put(headerParts[0].trim(),headerParts[1].trim());
+                 }
+             }
+             str=headers.get("User-Agent");
+             len=str.length();
              String responseBody= "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+len+"\r\n\r\n"+str;
              System.out.println(responseBody);
              socket.getOutputStream().write(responseBody.getBytes());
