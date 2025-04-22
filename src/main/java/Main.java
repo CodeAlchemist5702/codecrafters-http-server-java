e1768fb0e0a125225a27265a1bbbc5845b43bc3d
anurag <anuragsharmajobmail@gmail.com>
2025-04-22 22:42:37 +0530

    http program2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,10 +16,24 @@ public class Main {
 
        // Since the tester restarts your program quite often, setting SO_REUSEADDR
        // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+         serverSocket.setReuseAddress(true);
         Socket socket = serverSocket.accept();
-        socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-       System.out.println("accepted new connection");
+         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+         String requestLine = br.readLine();
+         String path ="/";
+         if(requestLine!=null){
+             String [] parts = requestLine.split("");
+             if (parts.length >= 2) {
+                 path = parts[1];
+             }
+         }
+         if("/".equals(path)) {
+             socket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+         }
+         else{
+             socket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+         }
+       System.out.println("           new connection");
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
